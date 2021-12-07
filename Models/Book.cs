cece985fa08a52a4ecad9f136e9bd69b21c1150f
Tarau Bianca <tarau_bianca@yahoo.com>
2021-12-07 15:33:28 +0200

    Lab9

--- Models/Book.cs
+++ Models/Book.cs
@@ -5,15 +5,19 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
 namespace Tarau_Bianca_Lab8.Models
 {
     public class Book
     {
         public int ID { get; set; }
 
+        [Required, StringLength(150, MinimumLength = 3)]
         [Display(Name = "Book Title")]
         public string Title { get; set; }
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma 'Prenume Nume'"),  Required, StringLength(50, MinimumLength = 3)]
         public string Author { get; set; }
+        [Range(1, 300)]
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
         [DataType(DataType.Date)]
